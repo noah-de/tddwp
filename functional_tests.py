7bf1761c0ecb7f9f24eb65cc90112a5b749589f3
Noah Spahn <noah.de@gmail.com>
2014-05-05 20:29:45 -0700

    refactored the functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Herbert came across a to-do app that was on his own desktop. 
         # He finds it at localhost.
@@ -33,16 +39,7 @@ class NewVisitorTest(unittest.TestCase):
         # When he hits enter, the page updates and now the page lists
         # "1: buy potatoes" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        import time
-        #time.sleep(3)
-        self.assertTrue(
-           any(row.text == '1. buy potatoes' for row in rows),
-           "New to-do item did not appear in table -- its text was: \n%s" % (
-               table.text,)
-        )
+        self.check_for_row_in_list_table('1. buy potatoes')
         
         # There is still a text box inviting him to add another item
         # He enters: "wash and peel potatoes" as an item.
@@ -51,10 +48,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         
         # The page updates again, and now shows both items on the list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1. buy potatoes', [row.text for row in rows])
-        self.assertIn('wash and peel potatoes', [row.text for row in rows])
+        self.check_for_row_in_list_table('1. buy potatoes')
+        self.check_for_row_in_list_table('2. wash and peel potatoes')
 
         self.fail('Finish the test')
 
