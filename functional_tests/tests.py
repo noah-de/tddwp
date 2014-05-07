b2b4d35a9ad8e4d04a9c422551fcad33c3820796
Noah Spahn <noah.de@gmail.com>
2014-05-06 21:29:38 -0700

    modified the template, gitignore and functional tests now useLiveServerTestCase and reside in functional_tests/tests.py

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,9 @@
 #!/usr/local/bin/python3
-
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -22,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Herbert came across a to-do app that was on his own desktop. 
         # He finds it at localhost.
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         
         # He sees that the page title mentions to-do lists
         self.assertIn('To-Do', self.browser.title)
