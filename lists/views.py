bf3cb4e5108676aa4e3e1c645a2ddac220c9dade
Noah Spahn <noah.de@gmail.com>
2014-05-11 16:51:01 -0700

    new url and view for adding to existing lists. All tests now pass

--- lists/views.py
+++ lists/views.py
@@ -5,11 +5,16 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'home.html')
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items':items})
+def view_list(request, list_id):
+    listo = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list':listo})
 
 def new_list(request):
     listo = List.objects.create()
     Item.objects.create(text=request.POST['item_text'],list=listo)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect('/lists/%d/' % (listo.id,))
+
+def add_item(request,list_id):
+    listo = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'],list=listo)
+    return redirect('/lists/%d/' % (listo.id,))
