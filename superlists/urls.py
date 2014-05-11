9dab30a560c17c21dfda30fe51b18bb41a2cc5e4
Noah Spahn <noah.de@gmail.com>
2014-05-11 16:59:35 -0700

    fixed urls (post testing)

--- superlists/urls.py
+++ superlists/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/(\d+)/$', include('lists.urls')),
+    url(r'^lists/', include('lists.urls')),
     # url(r'^admin/', include(admin.site.urls)),
 )
